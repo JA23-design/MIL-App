7354ad040fcba3f8b8a101314d6969bcaabdc958
JA23-design <jovaugn.m.cox@gmail.com>
2026-06-10 16:25:03 -0400

    Add User and FamilyProfile models to Models.py

--- Models.py
+++ Models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from extensions import db
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -25,3 +27,18 @@ class ChildcareCenter(db.Model):
     accepts_ccys = db.Column(db.Boolean, default=False)
     age_range = db.Column(db.String(50))
     availability_status = db.Column(db.String(50))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(255))
+
+class FamilyProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    base_id = db.Column(db.Integer, db.ForeignKey("base.id"))
+
+    has_children = db.Column(db.Boolean, default=False)
+    child_age_range = db.Column(db.String(50))
+    spouse_needs_healthcare = db.Column(db.Boolean, default=True)
