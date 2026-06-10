a64f9fe984369fb482495d39ae21ed724e5c4c60
JA23-design <jovaugn.m.cox@gmail.com>
2026-06-10 16:10:23 -0400

    Implement live search suggestions for bases

--- static/apps.js
+++ static/apps.js
@@ -1,1 +1,23 @@
+const input = document.getElementById("baseSearch");
+const box = document.getElementById("suggestions");
+const hidden = document.getElementById("base_id");
 
+input.addEventListener("input", async () => {
+    const res = await fetch(`/api/search/bases?q=${input.value}`);
+    const data = await res.json();
+
+    box.innerHTML = "";
+
+    data.forEach(b => {
+        const div = document.createElement("div");
+        div.innerText = `${b.name} (${b.state})`;
+
+        div.onclick = () => {
+            input.value = b.name;
+            hidden.value = b.id;
+            box.innerHTML = "";
+        };
+
+        box.appendChild(div);
+    });
+});
