af49346fc9aa4cb05278e66ba074ce59cb233009
JA23-design <jovaugn.m.cox@gmail.com>
2026-09-21 09:25:13 -0400

    Update SECRET_KEY and DATABASE_URL in config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-change-me")
-
-    SQLALCHEMY_DATABASE_URI = os.getenv(
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-change-this-secret")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///instance/app.db"
+        "sqlite:///mil_app.db"
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
